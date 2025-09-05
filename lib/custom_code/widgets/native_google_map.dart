210c0bf674f917277d64515739c49a80567f35cd
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-05 08:24:35 -0300

    fix: import PlatformViewHitTestBehavior

--- lib/custom_code/widgets/native_google_map.dart
+++ lib/custom_code/widgets/native_google_map.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 import '/flutter_flow/lat_lng.dart';
 
 typedef NativeMapCreatedCallback = void Function(NativeGoogleMapController controller);
