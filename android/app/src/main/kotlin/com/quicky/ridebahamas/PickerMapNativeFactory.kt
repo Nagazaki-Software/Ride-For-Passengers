14813d8ddcabf30314454b447c9f147e466eecb0
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-09 18:38:33 -0300

    Update PickerMapNativeFactory.kt

--- android/app/src/main/kotlin/com/quicky/ridebahamas/PickerMapNativeFactory.kt
+++ android/app/src/main/kotlin/com/quicky/ridebahamas/PickerMapNativeFactory.kt
@@ -1,3 +1,4 @@
+// android/app/src/main/kotlin/com/quicky/ridebahamas/PickerMapNativeFactory.kt
 package com.quicky.ridebahamas
 
 import android.content.Context
@@ -7,9 +8,10 @@ import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
 class PickerMapNativeFactory(private val messenger: BinaryMessenger) :
-    PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+  PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+
   override fun create(context: Context, id: Int, obj: Any?): PlatformView {
+    // Se quiser ler creationParams, estão em `obj`, mas seu Dart manda só initialUserLocation para uso na view
     return PickerMapNativeView(context, messenger, id)
   }
 }
-
