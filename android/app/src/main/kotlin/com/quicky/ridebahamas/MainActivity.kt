12a099d9b1c6fbedac2d6cb5d0fa3558a13e7741
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-10 10:38:04 -0300

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/quicky/ridebahamas/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.quicky.ridebahamas
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    flutterEngine
+      .platformViewsController
+      .registry
+      .registerViewFactory(
+        "picker_map_native", // TEM que bater com o viewType no Dart
+        PickerMapNativeFactory(flutterEngine.dartExecutor.binaryMessenger)
+      )
+  }
+}
