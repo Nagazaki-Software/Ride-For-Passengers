6f449a24a1b383ac17af68ecbdd6025bf08bd8d3
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-10 19:08:18 -0300

    Update PickerMapNativeFactory.kt

--- android/app/src/main/kotlin/com/quicky/ridebahamas/PickerMapNativeFactory.kt
+++ android/app/src/main/kotlin/com/quicky/ridebahamas/PickerMapNativeFactory.kt
@@ -1,6 +1,7 @@
 package com.quicky.ridebahamas
 
 import android.content.Context
+import android.util.Log
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
@@ -12,6 +13,7 @@ class PickerMapNativeFactory(
 ) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
   override fun create(context: Context?, id: Int, args: Any?): PlatformView {
+    Log.d("PickerMap", "Factory.create(id=$id) args=$args")
     @Suppress("UNCHECKED_CAST")
     val creationParams = args as? Map<String, Any?> ?: emptyMap()
     return PickerMapNativeView(appContext, messenger, id, creationParams)
