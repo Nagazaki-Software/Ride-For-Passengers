5b55768add5843ad06f831b56f38249873594a9b
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-10 19:07:44 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/quicky/ridebahamas/MainActivity.kt
+++ android/app/src/main/kotlin/com/quicky/ridebahamas/MainActivity.kt
@@ -2,16 +2,19 @@ package com.quicky.ridebahamas
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import android.util.Log
 
 class MainActivity : FlutterActivity() {
   override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
+    Log.d("PickerMap", "MainActivity.configureFlutterEngine()")
     flutterEngine.platformViewsController.registry.registerViewFactory(
-      "picker_map_native", // <-- TEM QUE bater com o Dart
+      "picker_map_native",
       PickerMapNativeFactory(
         applicationContext,
         flutterEngine.dartExecutor.binaryMessenger
       )
     )
+    Log.d("PickerMap", "Factory registrada com viewType=picker_map_native")
   }
 }
