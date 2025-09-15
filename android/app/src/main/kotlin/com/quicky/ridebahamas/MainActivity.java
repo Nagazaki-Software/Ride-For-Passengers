2606d6d8a20e0b7b3db2014c02405ff722e2002f
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-15 11:03:14 -0300

    Update MainActivity.java

--- android/app/src/main/kotlin/com/quicky/ridebahamas/MainActivity.java
+++ android/app/src/main/kotlin/com/quicky/ridebahamas/MainActivity.java
@@ -2,17 +2,18 @@ package com.quicky.ridebahamas;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.platform.PlatformViewRegistry;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    public void configureFlutterEngine(FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        flutterEngine
-                .getPlatformViewsController()
-                .getRegistry()
-                .registerViewFactory(
-                        "picker_map_native",
-                        new PickerMapNativeFactory(flutterEngine.getDartExecutor().getBinaryMessenger())
-                );
-    }
+  @Override
+  public void configureFlutterEngine(FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    BinaryMessenger messenger = flutterEngine.getDartExecutor().getBinaryMessenger();
+    PlatformViewRegistry registry = flutterEngine.getPlatformViewsController().getRegistry();
+    registry.registerViewFactory(
+        "picker_map_native",
+        new PickerMapNativeFactory(messenger)
+    );
+  }
 }
