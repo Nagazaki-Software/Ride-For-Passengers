e3cae5b6c893f6488502cec616c703b5e5d8073f
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-25 23:48:22 -0300

    gmaps brasinree

---
+++ ios/Runner/GoogleMapsNativeSdkPluginStub.m
@@ -0,0 +1,11 @@
+#import <Flutter/Flutter.h>
+
+@interface GoogleMapsNativeSdkPlugin : NSObject
+@end
+
+@implementation GoogleMapsNativeSdkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  // No-op stub to satisfy linker when iOS sources are excluded
+}
+@end
+
