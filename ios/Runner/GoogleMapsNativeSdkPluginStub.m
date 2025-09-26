c79405331a6d1201779d857f2d7b57bafc676f89
Lucas César Lopes <lucascesarlopes@nagazakisoftware.com.br>
2025-09-26 12:07:51 -0300

    no 3ds

--- ios/Runner/GoogleMapsNativeSdkPluginStub.m
+++ ios/Runner/GoogleMapsNativeSdkPluginStub.m
@@ -1,11 +1,7 @@
-#import <Flutter/Flutter.h>
-
-@interface GoogleMapsNativeSdkPlugin : NSObject
-@end
+#import "../google_maps_native_sdk/GoogleMapsNativeSdkPlugin.h"
 
 @implementation GoogleMapsNativeSdkPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   // No-op stub to satisfy linker when iOS sources are excluded
 }
 @end
-
---
+++ ios/google_maps_native_sdk/GoogleMapsNativeSdkPlugin.h
@@ -0,0 +1,8 @@
+// Stub header to satisfy GeneratedPluginRegistrant imports when the
+// google_maps_native_sdk iOS sources are excluded from build.
+#import <Flutter/Flutter.h>
+
+@interface GoogleMapsNativeSdkPlugin : NSObject<FlutterPlugin>
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
+@end
+
